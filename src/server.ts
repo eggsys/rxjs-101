19b9b0cd7efb1d3475624eb49e60ad54ea03b8ab
eggsys <egg.rkt2@gmail.com>
2022-08-13 03:56:12 +0700

    implement observable

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,32 @@
-console.log("This is beginning");
+import { Observable } from "rxjs";
+
+const observable = new Observable((subscriber)=> {
+	subscriber.next(10);
+    subscriber.next(11);
+    subscriber.next(12);
+});
+
+
+const observer = {
+	next: (value: any)=>{
+		console.log("observer got a value of", value);
+    },
+    error: (value: any)=>{
+		console.log("observer got an error of", value);
+    },
+    complete: (value: any)=>{
+        console.log("observer got a complete notification", value);
+    }
+};  
+
+
+
+observable.subscribe(observer);
+
+// ## SAME RESULT ##
+
+// observable.subscribe({
+//     next: (value: any)=>{
+//         console.log("This what we got : ", value);
+//     }
+// });
